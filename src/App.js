55ebf8fb894ddbf8fec69f5991971749389dcdb1
Duy Bao <duybao.dragon@gmail.com>
2021-11-17 23:26:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import React, {fragment} from 'react'
+import Header from "./components/Header"
+import Todos from "./components/Todos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.js';
+
+const App = () => {
+    return (
+      <fragment>
+        
+        <Header/>
+        <Todos/>
+    
+      </fragment>
+    )
+      
+      
 }
 
 export default App;
